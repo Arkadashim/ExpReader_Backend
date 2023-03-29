a9879b7bbaf2265598f5dcda136371d15b8718e8
Arkadashim <andrewsha.antonov@mail.ru>
2023-03-29 12:12:24 +0300

    add ShowFav function

--- controllers/fav.js
+++ controllers/fav.js
@@ -2,6 +2,9 @@ const eH = require('../middleware/errorHandler');
 const { Sequelize } = require('../models');
 
 const Favorite = require('../models/favorite').Model;
+const Book = require('../models/book').Model;
+const Author = require('../models/author').Model;
+const BookAuthor = require('../models/bookauthor').Model;
 
 // should contain in req: in header - Authorization, in body - bookId
 module.exports.switchFav = async function (req, res) {
@@ -29,7 +32,40 @@ module.exports.switchFav = async function (req, res) {
 // should contain in req: in header - Authorization
 module.exports.showFav = async function(req, res) {
     try {
-        
+        const uId = req.user.id;
+
+        const _fav = await Favorite.findAll({
+            raw: true,
+            where: {UserId: uId},
+            include: {model: Book, attributes: []},
+            attributes: [
+                [Sequelize.col('Book.id'), 'id'],
+                [Sequelize.col('Book.title'), 'title'],
+                [Sequelize.col('Book.cover'), 'cover']
+            ]
+        });
+
+        for (var i in _fav) {
+            const authors = await BookAuthor.findAll({
+                raw: true,
+                where: { BookId: _fav[i].id },
+                include: [{
+                    model: Author,
+                    attributes: []
+                }],
+                attributes: [
+                    [Sequelize.col('Author.name'), 'name']
+                ]
+            });
+
+            _fav[i].authors = [];
+
+            for (var j in authors) {
+                _fav[i].authors.push(authors[j].name);
+            }    
+        }
+
+        res.status(200).json(_fav);
     } catch(err) {
         eH(res, err);
     }
