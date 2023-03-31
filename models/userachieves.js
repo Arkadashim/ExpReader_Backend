d7d0a82f35afc9a28a9559b9678ae8df53df6330
Arkadashim <andrewsha.antonov@mail.ru>
2023-03-31 15:52:13 +0300

    update the model

--- models/userachieves.js
+++ models/userachieves.js
@@ -16,5 +16,6 @@ module.exports = (sequelize, DataTypes) => {
         timestamps: false,
         modelName: 'UserAchieves',
     });
+    module.exports.Model = UserAchieves;
     return UserAchieves;
 };
