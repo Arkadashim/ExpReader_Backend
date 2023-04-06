720b5f5ebe7dedf89c40b859fbc741f2f3a64b2d
Arkadashim <andrewsha.antonov@mail.ru>
2023-04-06 15:17:21 +0300

    add new components

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const controller  = require('../controllers/user');
+
+
+
+module.exports = router;
