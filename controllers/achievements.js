cea5bbf86716cfe684ee6b771bcb0c0756be985e
Arkadashim <andrewsha.antonov@mail.ru>
2023-04-17 14:11:07 +0300

    temporary off controller

--- controllers/achievements.js
+++ controllers/achievements.js
@@ -1,18 +1,18 @@
 const eH = require('../middleware/errorHandler');
 const { Sequelize } = require('../models');
 
-const Achievement = require('../models/achievement').Model;
-const UserAchieve = require('../models/userachieves').Model;
+// const Achievement = require('../models/achievement').Model;
+// const UserAchieve = require('../models/userachieves').Model;
 
 // auth + achieveID in body
 module.exports.completeAchievement = async function (req, res) {
     try {
-        const uID = req.user.id;
-        const aID = req.body.achieveID;
+        // const uID = req.user.id;
+        // const aID = req.body.achieveID;
 
-        await UserAchieve.findOrCreate({ where: { UserId: uID, AchievementId: aID } });
+        // await UserAchieve.findOrCreate({ where: { UserId: uID, AchievementId: aID } });
 
-        res.status(200).end();
+        // res.status(200).end();
     } catch (err) {
         eH(res, err);
     }
@@ -21,21 +21,21 @@ module.exports.completeAchievement = async function (req, res) {
 // auth in header
 module.exports.getUserAchievements = async function (req, res) {
     try {
-        const uID = req.user.id;
+        // const uID = req.user.id;
 
-        const _ach = await UserAchieve.findAll({
-            raw: true,
-            where: { UserId: uID },
-            include: { model: Achievement, attributes: [] },
-            attributes: [
-                [Sequelize.col('Achievement.id'), 'id'],
-                [Sequelize.col('Achievement.title'), 'title'],
-                [Sequelize.col('Achievement.description'), 'description'],
-                [Sequelize.col('Achievement.image'), 'image']
-            ]
-        });
+        // const _ach = await UserAchieve.findAll({
+        //     raw: true,
+        //     where: { UserId: uID },
+        //     include: { model: Achievement, attributes: [] },
+        //     attributes: [
+        //         [Sequelize.col('Achievement.id'), 'id'],
+        //         [Sequelize.col('Achievement.title'), 'title'],
+        //         [Sequelize.col('Achievement.description'), 'description'],
+        //         [Sequelize.col('Achievement.image'), 'image']
+        //     ]
+        // });
 
-        res.status(200).json(_ach);
+        // res.status(200).json(_ach);
     } catch (err) {
         eH(res, err);
     }
@@ -44,14 +44,14 @@ module.exports.getUserAchievements = async function (req, res) {
 module.exports.getAllAchievements = async function (req, res) {
     try {
 
-        const _ach = await Achievement.findAll({
-            raw: true,
-            attributes: [
-                'id', 'title', 'description', 'image'
-            ]
-        });
+        // const _ach = await Achievement.findAll({
+        //     raw: true,
+        //     attributes: [
+        //         'id', 'title', 'description', 'image'
+        //     ]
+        // });
 
-        res.status(200).json(_ach);
+        // res.status(200).json(_ach);
     } catch (err) {
         eH(res, err);
     }
