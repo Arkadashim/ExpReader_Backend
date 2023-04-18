de920a1caea0a0bd89118f6f4058654e0288f918
Arkadashim <andrewsha.antonov@mail.ru>
2023-04-18 21:02:25 +0300

    drop sync

--- sync.js
+++
@@ -1,9 +0,0 @@
-const Models = require('./models');
-
-Models.sequelize.sync({
-    alter: true,
-    logging: console.log
-}).then(() => 
-    console.log("Ok"))
-.catch((err) => 
-    console.log(err));
